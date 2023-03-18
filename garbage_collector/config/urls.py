8067491f2913b5dd8c278e28e5fa6aded4d467d4
liskunovich <david.leonov.96@mail.ru>
2023-03-17 23:23:09 -0400

    Made small fixes, renamed the folder

--- garbage_collector/config/urls.py
+++ garbage_collector/config/urls.py
@@ -1,4 +1,4 @@
-"""garbage_collector URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/main/', include('main.urls')),
 ]
